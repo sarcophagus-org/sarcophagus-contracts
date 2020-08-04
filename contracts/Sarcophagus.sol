f2d6a198334049cecf99c9d5d72bfb1cad90e831
Adam Gall <adam@decentlabs.io>
2020-08-04 16:11:04 -0400

    Register Archaeologists (#2)
    * Require that a non-empty public key is input into register function
    
    * Remove the ganache-time-traveler package
    
    * Finish creating registration function and writing tests

--- contracts/Sarcophagus.sol
+++ contracts/Sarcophagus.sol
@@ -2,5 +2,60 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract Sarcophagus {
-  function register() public returns (bool) {}
+
+  event RegisterArchaeologist(
+    bytes publicKey,
+    address paymentAddress,
+    uint minimumBounty,
+    uint minimumDiggingFee,
+    uint maximumResurrectionTime,
+    uint bond
+  );
+
+  struct Archaeologist {
+    address paymentAddress;
+    uint minimumBounty;
+    uint minimumDiggingFee;
+    uint maximumResurrectionTime;
+    uint bond;
+  }
+
+  mapping(bytes => Archaeologist) public archaeologists;
+  bytes[] public archaeologistKeys;
+
+  function archaeologistCount() public view returns (uint) {
+    return archaeologistKeys.length;
+  }
+
+  function register(
+    bytes memory publicKey,
+    uint minimumBounty,
+    uint minimumDiggingFee,
+    uint maximumResurrectionTime
+  ) public payable returns (bool) {
+    require(archaeologists[publicKey].paymentAddress == address(0), "archaeologist already registered");
+    require(publicKey.length == 64, "public key must be 64 bytes");
+
+    Archaeologist memory newArch = Archaeologist(
+      msg.sender,
+      minimumBounty,
+      minimumDiggingFee,
+      maximumResurrectionTime,
+      msg.value
+    );
+
+    archaeologists[publicKey] = newArch;
+    archaeologistKeys.push(publicKey);
+
+    emit RegisterArchaeologist(
+      publicKey,
+      newArch.paymentAddress,
+      newArch.minimumBounty,
+      newArch.minimumDiggingFee,
+      newArch.maximumResurrectionTime,
+      newArch.bond
+    );
+
+    return true;
+  }
 }
