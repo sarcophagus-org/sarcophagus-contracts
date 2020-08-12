b0b4537ad859a4dd9f59d1800d60c0fdc1b18100
Adam Gall <adam@decentlabs.io>
2020-08-12 14:04:30 -0400

    Feature/openzeppelin imports (#16)
    * Add openzeppelin contracts as an npm dev dependency
    
    * Use the openzeppelin imports instead of manually added openzeppelin contracts

--- contracts/Sarcophagus.sol
+++ contracts/Sarcophagus.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "./math/SafeMath.sol";
-import "./token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Sarcophagus {
     using SafeMath for uint256;
--- contracts/SarcophagusToken.sol
+++ contracts/SarcophagusToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "./token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SarcophagusToken is ERC20 {
     constructor(
