9172aff81771a0f2e1b9fcfe7b63a267526e1f4f
Adam Gall <adam@decentlabs.io>
2020-11-03 15:00:29 -0500

    Fix the tests

--- test/PrivateKeys.test.ts
+++ test/PrivateKeys.test.ts
@@ -1,17 +1,17 @@
 import { expect, use } from "chai"
 import { Contract } from "ethers"
 import { deployContract, MockProvider, solidity } from "ethereum-waffle"
-import ECSol from "../build/EC.json"
+import PrivateKeys from "../build/PrivateKeys.json"
 
 use(solidity)
 
 describe("Private Keys", () => {
   const provider = new MockProvider()
   const [wallet] = provider.getWallets()
-  let ecsol: Contract;
+  let privateKeys: Contract;
 
   beforeEach(async () => {
-    ecsol = await deployContract(wallet, ECSol)
+    privateKeys = await deployContract(wallet, PrivateKeys)
   })
 
   it("verifies correct key", async () => {    
@@ -19,7 +19,7 @@ describe("Private Keys", () => {
     const privKey = Buffer.from([137, 238, 6, 7, 23, 118, 40, 25, 176, 223, 165, 1, 209, 243, 194, 70, 240, 89, 253, 112, 161, 181, 76, 114, 49, 230, 23, 179, 37, 148, 229, 85])
     const pubKey = Buffer.from([233, 45, 247, 2, 81, 26, 248, 250, 30, 239, 17, 42, 103, 144, 255, 102, 13, 145, 39, 231, 109, 107, 253, 154, 66, 224, 30, 50, 163, 116, 223, 60, 208, 214, 158, 193, 103, 118, 26, 81, 147, 29, 173, 153, 211, 187, 161, 129, 43, 25, 132, 34, 67, 4, 80, 121, 41, 188, 19, 250, 232, 211, 246, 137])
 
-    const valid = await ecsol.publicKeyVerify(privKey, pubKey)
+    const valid = await privateKeys.keyVerification(privKey, pubKey)
     expect(valid).to.be.true
   })
 })
