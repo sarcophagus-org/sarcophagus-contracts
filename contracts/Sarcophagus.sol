78012fb58328ace5cf23ec99d3f822cb381fb7f9
Adam Gall <adam@decentlabs.io>
2021-03-25 09:51:15 -0400

    Update oz (#40)
    * Add version 4.0.0 of openzeppelin libraries
    
    * Use those official OZ imports instead of manually added dependencies
    
    * Remove unused manually added/changed OZ libraries

--- contracts/Sarcophagus.sol
+++ contracts/Sarcophagus.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
-import "./openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "./libraries/Events.sol";
 import "./libraries/Types.sol";
--- contracts/mocks/SarcoTokenMock.sol
+++ contracts/mocks/SarcoTokenMock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SarcoTokenMock is ERC20 {
     constructor() ERC20("SARCOMock", "Sarcophagus Mock") {
