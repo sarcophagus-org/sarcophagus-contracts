9b98dc7e205e197a552797cca86c822b9e7afc1b
Adam Gall <adam@decentlabs.io>
2021-03-29 21:23:26 -0400

    Mainnet (#45)
    * Fix migration async deployer calls
    
    * Deploy to Mainnet

--- migrations/2_link_and_deploy_libraries.js
+++ migrations/2_link_and_deploy_libraries.js
@@ -3,16 +3,16 @@ const PrivateKeys = artifacts.require("PrivateKeys")
 const Archaeologists = artifacts.require("Archaeologists")
 const Sarcophaguses = artifacts.require("Sarcophaguses")
 
-module.exports = async function (deployer) {
-  await deployer.deploy(Utils)
+module.exports = function (deployer) {
+  deployer.deploy(Utils)
 
   deployer.link(Utils, Archaeologists)
-  await deployer.deploy(Archaeologists)
+  deployer.deploy(Archaeologists)
 
-  await deployer.deploy(PrivateKeys)
+  deployer.deploy(PrivateKeys)
 
   deployer.link(Utils, Sarcophaguses)
   deployer.link(Archaeologists, Sarcophaguses)
   deployer.link(PrivateKeys, Sarcophaguses)
-  await deployer.deploy(Sarcophaguses)
+  deployer.deploy(Sarcophaguses)
 }
